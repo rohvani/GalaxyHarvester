afe452eee0c76d7698f52f08be951a8c12c87ea2
R. Uvanni <rezen1911@gmail.com>
2014-08-20 18:33:30 -0400

    Fixed indentation

--- galaxyHarvestor/Program.cs
+++ galaxyHarvestor/Program.cs
@@ -16,63 +16,62 @@ namespace galaxyHarvestor
 {
 	class Program
 	{
-        static void Main(string[] args)
-        {
-            Console.Title = "GalaxyHarvester Uploader";
+		static void Main(string[] args)
+		{
+			Console.Title = "GalaxyHarvester Uploader";
 
-            if (!File.Exists("config.cfg"))
-            {
-                Console.WriteLine("Couldn't find 'config.cfg' in local directory, is it missing?");
-            }
-            else if (!File.Exists("resource_manager_spawns.lua"))
-            {
-                Console.WriteLine("Couldn't find 'resource_manager_spawns.lua' in local directory, is it missing?");
-            }
-            else
-            {
-                string galaxyId = Config.getValue("galaxyId");
-                string cookie = Config.getValue("cookie");
+			if (!File.Exists("config.cfg"))
+			{
+				Console.WriteLine("Couldn't find 'config.cfg' in local directory, is it missing?");
+			}
+			else if (!File.Exists("resource_manager_spawns.lua"))
+			{
+				Console.WriteLine("Couldn't find 'resource_manager_spawns.lua' in local directory, is it missing?");
+			}
+			else
+			{
+				string galaxyId = Config.getValue("galaxyId");
+				string cookie = Config.getValue("cookie");
 
-                Console.WriteLine("Loading resources from 'resource_manager_spawns.lua'");
-                List<Resource> resources = SerializeResources();
+				Console.WriteLine("Loading resources from 'resource_manager_spawns.lua'");
+				List<Resource> resources = SerializeResources();
 
-                foreach (Resource resource in resources)
-                {
-                    using (var wb = new WebClient())
-                    {
-                        var data = new NameValueCollection();
+				foreach (Resource resource in resources)
+				{
+					using (var wb = new WebClient())
+					{
+						var data = new NameValueCollection();
 
-                        data["gh_sid"] = "'%2Bsid%2B'";
-                        data["galaxy"] = galaxyId;
-                        data["planet"] = resource.planet.ToString();
-                        data["resName"] = resource.name;
-                        data["resType"] = resource.type;
-                        data["sourceRow"] = "0";
-                        data["CR"] = resource.res_cold_resist == 0 ? "" : resource.res_cold_resist.ToString();
-                        data["CD"] = resource.res_conductivity == 0 ? "" : resource.res_conductivity.ToString();
-                        data["DR"] = resource.res_decay_resist == 0 ? "" : resource.res_decay_resist.ToString();
-                        data["FL"] = resource.res_flavor == 0 ? "" : resource.res_flavor.ToString();
-                        data["HR"] = resource.res_heat_resist == 0 ? "" : resource.res_heat_resist.ToString();
-                        data["MA"] = resource.res_malleability == 0 ? "" : resource.res_malleability.ToString();
-                        data["PE"] = resource.res_potential_energy == 0 ? "" : resource.res_potential_energy.ToString();
-                        data["OQ"] = resource.res_quality == 0 ? "" : resource.res_quality.ToString();
-                        data["SR"] = resource.res_shock_resistance == 0 ? "" : resource.res_shock_resistance.ToString();
-                        data["UT"] = resource.res_toughness == 0 ? "" : resource.res_toughness.ToString();
+						data["gh_sid"] = "'%2Bsid%2B'";
+						data["galaxy"] = galaxyId;
+						data["planet"] = resource.planet.ToString();
+						data["resName"] = resource.name;
+						data["resType"] = resource.type;
+						data["sourceRow"] = "0";
+						data["CR"] = resource.res_cold_resist == 0 ? "" : resource.res_cold_resist.ToString();
+						data["CD"] = resource.res_conductivity == 0 ? "" : resource.res_conductivity.ToString();
+						data["DR"] = resource.res_decay_resist == 0 ? "" : resource.res_decay_resist.ToString();
+						data["FL"] = resource.res_flavor == 0 ? "" : resource.res_flavor.ToString();
+						data["HR"] = resource.res_heat_resist == 0 ? "" : resource.res_heat_resist.ToString();
+						data["MA"] = resource.res_malleability == 0 ? "" : resource.res_malleability.ToString();
+						data["PE"] = resource.res_potential_energy == 0 ? "" : resource.res_potential_energy.ToString();
+						data["OQ"] = resource.res_quality == 0 ? "" : resource.res_quality.ToString();
+						data["SR"] = resource.res_shock_resistance == 0 ? "" : resource.res_shock_resistance.ToString();
+						data["UT"] = resource.res_toughness == 0 ? "" : resource.res_toughness.ToString();
 						data["ER"] = resource.entangle_resistance == 0 ? "" : resource.entangle_resistance.ToString();
 
-                        wb.Headers.Add(HttpRequestHeader.Cookie, cookie);
-                       // var response = wb.UploadValues(new Uri("http://www.galaxyharvester.net/postResource.py"), "POST", data);
-                    }
-                    Console.SetCursorPosition(0, 1);
+						wb.Headers.Add(HttpRequestHeader.Cookie, cookie);
+						// var response = wb.UploadValues(new Uri("http://www.galaxyharvester.net/postResource.py"), "POST", data);
+					}
+					Console.SetCursorPosition(0, 1);
 					Console.WriteLine(resource.res_quality);
-                    Console.WriteLine("[{0}/{1}] Uploading resources to GalaxyHarvester", resources.IndexOf(resource), resources.Count);
-                    Thread.Sleep(10);
-                }
-
-                Console.WriteLine("Completed uploading {0} resources to GalaxyHarvester!", resources.Count);
-            }
-            Console.Read();
-        }
+					Console.WriteLine("[{0}/{1}] Uploading resources to GalaxyHarvester", resources.IndexOf(resource), resources.Count);
+					Thread.Sleep(10);
+				}
+				Console.WriteLine("Completed uploading {0} resources to GalaxyHarvester!", resources.Count);
+			}
+			Console.Read();
+		}
 
 		static List<Resource> SerializeResources()
 		{
@@ -84,12 +83,12 @@ namespace galaxyHarvestor
 			foreach (DictionaryEntry resource in lua.GetTable("resources"))
 			{
 				Resource res = new Resource();
-				foreach (DictionaryEntry stat in lua.GetTableDict((LuaTable) resource.Value))
+				foreach (DictionaryEntry stat in lua.GetTableDict((LuaTable)resource.Value))
 				{
-					switch((String) stat.Key)
+					switch ((String)stat.Key)
 					{
-						case "name": 
-							res.name = (String) stat.Value;
+						case "name":
+							res.name = (String)stat.Value;
 							break;
 
 						case "type":
@@ -110,7 +109,7 @@ namespace galaxyHarvestor
 							break;
 
 						case "zoneRestriction":
-							switch((String) stat.Value)
+							switch ((String)stat.Value)
 							{
 								case "corellia":
 									res.planet = 1;
--- galaxyHarvestor/Resource.cs
+++ galaxyHarvestor/Resource.cs
@@ -10,17 +10,17 @@ namespace galaxyHarvestor
 	{
 		public int planet = 0;
 		public string name = "none";
-        public string type = "none";
+		public string type = "none";
 		public int res_cold_resist = 0;
-        public int res_conductivity = 0;
+		public int res_conductivity = 0;
 		public int res_decay_resist = 0;
 		public int res_flavor = 0;
-        public int res_heat_resist = 0;
-        public int res_malleability = 0;
-        public int res_quality = 0;
-        public int res_shock_resistance = 0;
-        public int res_toughness = 0;
-        public int res_potential_energy = 0;
+		public int res_heat_resist = 0;
+		public int res_malleability = 0;
+		public int res_quality = 0;
+		public int res_shock_resistance = 0;
+		public int res_toughness = 0;
+		public int res_potential_energy = 0;
 		public int entangle_resistance = 0;
 	}
 }
